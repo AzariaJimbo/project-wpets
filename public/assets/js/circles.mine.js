a533594fe5da332467f072692cb5ceced9a9a987
AzariaJimbo <141146385+AzariaJimbo@users.noreply.github.com>
2023-09-11 21:11:38 +0300

    Add files via upload

---
+++ public/assets/js/circles.mine.js
@@ -0,0 +1,24 @@
+jQuery(function ($) {
+	// $('.progress1').circleProgress({
+	// 	max: 100,
+	// 	value: 100,
+	// 	textFormat: function (value, max) {
+	// 		return value + ' Watts';
+	// 	}
+	// })
+
+	// $('.progress2').circleProgress({
+	// 	max: 100,
+	// 	value: 0,
+	// 	textFormat: function (value, max) {
+	// 		return value + ' Watts';
+	// 	}
+	// })
+});
+
+
+
+
+
+
+
